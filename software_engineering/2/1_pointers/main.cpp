a28cba002494414af5c727c68c70fcc4c1d4e8d3
Kirill Mironov <zwugme@gmail.com>
2015-10-14 17:35:06 +0300

    added lab about pointers

---
+++ software_engineering/2/1_pointers/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+  long int value1, value2;
+  value1 = 200000;
+  long int * LPtr = &value1;
+  std::cout << value1 << " | pointer to value1 = " << LPtr << std::endl;
+  LPtr = &value2;
+  std::cout << value2 << " | pointer to value2 = " << LPtr << " | address of value1 = " << &value1 <<std::endl;
+}
