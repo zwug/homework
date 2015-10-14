5cebc15f45fdf8927ec65da708cafb8a6d8fa7f6
Kirill Mironov <zwugme@gmail.com>
2015-10-14 19:21:00 +0300

    added strings lab

---
+++ software_engineering/2/2_strings/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <typeinfo>
+
+int main() {
+  char *phone;
+  int locale = 0;
+  char * string_number;
+  long number;
+
+  phone = new char;
+  string_number = new char;
+
+  std::cout << "Type the phone..." << std::endl;
+  std::cin >> phone;
+  std::cout << phone << std::endl;
+
+  char * lexem = strtok(phone, " ()-");
+
+  while (lexem != NULL) {
+    if (locale == 0) {
+      locale = atoi(lexem);
+    } else {
+      string_number = strcat(string_number, lexem);
+    }
+
+    lexem = strtok (NULL, " ()-");
+  }
+
+  number = atoi(string_number);
+
+  std::cout << "Locale index = " << locale << " | typeid = " << typeid(locale).name() << std::endl;
+  std::cout << "Number = " << string_number << " | typeid = " << typeid(number).name() << std::endl;
+}
