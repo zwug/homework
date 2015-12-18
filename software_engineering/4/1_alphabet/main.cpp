6a959cf4ccd059a12720cdc91eff3f36961e10b1
Kirill <zwugme@gmail.com>
2015-12-18 21:40:27 +0300

    Update main.cpp

--- software_engineering/4/1_alphabet/main.cpp
+++ software_engineering/4/1_alphabet/main.cpp
@@ -3,39 +3,42 @@
 #include <vector>
 #include <string.h>
 
+const int SYMBOLS_COUNT = 26 * 2; // 25 сиволов, 24 запятые + символ конца строки
+
 int main() {
-  std::string fileContent;
-  std::vector <char> characters;
-  std::ifstream file ("alphabet.txt");
-  if (file.is_open()) {
-    getline(file, fileContent);
-    char *fileLine;
-    fileLine = new char [fileContent.length()];
-    // Копирование, так как strtok влияет на строку
-    strcpy(fileLine, fileContent.c_str());
-    char *letter = strtok(fileLine, ",");
+	char fileContent[SYMBOLS_COUNT];
+	std::vector <char> characters;
+	std::ifstream file("alphabet.txt");
+	if (file.is_open()) {
+		file >> fileContent;
+		char *next_symbol;
+		char *letter = strtok_s(fileContent, ",", &next_symbol);
 
-    for (int i = 0; letter != NULL; i++) {
-      if (*letter == 'p') {
-        break;
-      } else {
-        characters.push_back(*letter);
-        if(characters.size() == 11) {
-          characters.erase(characters.begin());
-        }
+		for (int i = 0; letter != NULL; i++) {
+			if (*letter == 'p') {
+				break;
+			}
+			else {
+				characters.push_back(*letter);
+				if (characters.size() == 11) {
+					characters.erase(characters.begin());
+				}
+			}
 
-      }
+			letter = strtok_s(NULL, ",", &next_symbol);
+		}
+		std::cout << "result array:" << std::endl;
+		for (int i = 0; i < characters.size(); i++) {
+			std::cout << characters[i] << " ";
+		}
+		std::cout << "\n";
+		file.close();
+	}
 
-      letter = strtok (NULL, ",");
-    }
-    std::cout << "result array:" << std::endl;
-    for (int i = 0; i < characters.size(); i++) {
-      std::cout << characters[i] << " ";
-    }
-    std::cout << "\n";
-    file.close();
-  }
-
-  else std::cout << "Unable to open file";
-  return 0;
+	else {
+		std::cout << "Unable to open file";
+		exit(1);
+	}
+	std::cin.get();
+	return 0;
 }
