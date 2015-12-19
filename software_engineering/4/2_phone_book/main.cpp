ee4fd112df6606fa9f25577e535d06905674ecac
Kirill <zwugme@gmail.com>
2015-12-19 15:25:32 +0300

    Update main.cpp

--- software_engineering/4/2_phone_book/main.cpp
+++ software_engineering/4/2_phone_book/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <cstdlib>
 #include <fstream>
 #include <string.h>
@@ -7,103 +8,129 @@ const int NAME_LENGTH = 25;
 const int PHONE_LENGTH = 15;
 const int ROW_LENGTH = NAME_LENGTH * 2 + PHONE_LENGTH;
 
-void generateName(char* name) {
-  const char* letters[2] = { "bcdfghjklmnpqrstvwxyz ", "aeiouy" };
-  const char letterlen[2] = { strlen(letters[0]), strlen(letters[1]) };
-  for (int i=0; i < NAME_LENGTH; i++) {
-    name[i] = letters[i%2][std::rand()%letterlen[i%2]];
-  }
-  name[0] = toupper(name[0]);
-}
-
 void generatePhone(char* number) {
-  const char* digits = "1234567890";
-  const char digitslen = strlen(digits);
-  for (int i=0; i < PHONE_LENGTH - 1; i++) {
-    switch (i) {
-      case 1:
-        number[i] = '(';
-        break;
-      case 5:
-        number[i] = ')';
-        break;
-      case 9:
-        number[i] = '-';
-        break;
-      default:
-        number[i] = digits[std::rand()%digitslen];
-        break;
-    }
-  }
+	const char* digits = "1234567890";
+	int digitslen = strlen(digits);
+	for (int i = 0; i < PHONE_LENGTH - 1; i++) {
+		switch (i) {
+		case 0:
+			number[i] = '8';
+			break;
+		case 1:
+			number[i] = '(';
+			break;
+		case 5:
+			number[i] = ')';
+			break;
+		case 9:
+			number[i] = '-';
+			break;
+		default:
+			number[i] = digits[std::rand() % digitslen];
+			break;
+		}
+	}
 }
 
-void printFile(std::fstream &file) {
-  std::cout << std::endl << "***************** Prinitng file ****************" << std::endl << std::endl;
-  std::string outputLine;
-  file.seekg (0, file.beg);
-  getline(file, outputLine);
-  std::cout << outputLine << std::endl;
-  std::cout << std::endl << "***************** end of file ****************" << std::endl << std::endl;
-  std::cout << "Length of the file should be " << ROW_LENGTH * 88 << ", got " << outputLine.length() << std::endl;
-  // Проверка форматирования
-  bool formatError = false;
-  for (int i = 0; i < 88; i++) {
-    int offset = i * ROW_LENGTH + 2 * NAME_LENGTH;
-    if(outputLine[offset + 1] != '(' || outputLine[offset + 5] != ')' || outputLine[offset + 9] != '-') {
-      formatError = true;
-      break;
-    }
-  }
-  if(formatError) {
-    std::cout << "Correct formatting" << std::endl;
-  } else {
-    std::cout << "Error: incorrect formatting" << std::endl;
-  }
+void printFile(std::fstream &file, const char filename[]) {
+	file.seekg(0, file.end);
+	int fileSize = file.tellg();
+	char *outputLine;
+	outputLine = new char[fileSize + 1];
+	std::ifstream fileToRead;
+	fileToRead.open(filename, std::ios::binary | std::ios::in);
+	if (fileToRead.fail()) {
+		std::cout << "Не удается открыть файл\n";
+		exit(1);
+	}
+	fileToRead.seekg(0, fileToRead.beg);
+	fileToRead.read(outputLine, file.tellg());
+	fileToRead.read(outputLine, file.tellg());
+	std::cout << std::endl << "***************** Prinitng file ****************" << std::endl << std::endl;
+	for (int i = 0, position = 0; i < fileSize; i++, position++) {
+		std::cout << outputLine[i];
+		if (position == ROW_LENGTH) {
+			position = 0;
+		}
+		switch (position) {
+		case NAME_LENGTH - 1:
+		case NAME_LENGTH * 2 - 1:
+			std::cout << " | ";
+			break;
+		case ROW_LENGTH - 1:
+			std::cout << "\n";
+		default:
+			break;
+		}
+	}
+	std::cout << std::endl << "***************** end of file ******************" << std::endl << std::endl;
+	std::cout << "Length of the file should be " << ROW_LENGTH * 88 << ", got " << fileSize << std::endl;
+	// Проверка форматирования
+	bool formatError = false;
+	for (int i = 0; i < 88; i++) {
+		int offset = i * ROW_LENGTH + 2 * NAME_LENGTH;
+		if (outputLine[offset + 1] != '(' || outputLine[offset + 5] != ')' || outputLine[offset + 9] != '-') {
+			formatError = true;
+			break;
+		}
+	}
+	if (!formatError) {
+		std::cout << "Correct formatting" << std::endl;
+	}
+	else {
+		std::cout << "Error: incorrect formatting" << std::endl;
+	}
+	delete[] outputLine;
+	fileToRead.close();
 }
 
-int CreateFormatFile(char * filename){
-  struct contact {
-    char firstName [NAME_LENGTH];
-    char lastName [NAME_LENGTH];
-    char phone[PHONE_LENGTH];
-  };
-  std::fstream file;
-  file.open(filename, std::ios::binary | std::ios::in | std::ios::out | std::ofstream::trunc);
-  if(!file){
-    std::cout << "Не удается открыть файл\n";
-    return 1;
-  }
-  // Запись пустых данных
-  file.seekg (0, file.beg);
-  for (int i = 0; i < 80; i++) {
-    contact emptyContact = {"", "", ""};
-    file.write((char* ) &emptyContact, sizeof(contact));
-  }
-  // Запись заполненных данных
-  file.seekg (0, file.end);
-  for (int i = 0; i < 8; i++) {
-    contact testContact;
-    generateName(testContact.firstName);
-    generateName(testContact.lastName);
-    generatePhone(testContact.phone);
-    file.write((char* ) &testContact, sizeof(contact));
-  }
+int CreateFormatFile(const char filename[]) {
+	struct contact {
+		char firstName[NAME_LENGTH];
+		char lastName[NAME_LENGTH];
+		char phone[PHONE_LENGTH];
+	};
+	std::fstream file;
+	file.open(filename, std::ios::binary | std::ios::out | std::ofstream::trunc);
+	if (!file) {
+		std::cout << "Не удается открыть файл\n";
+		return 1;
+	}
+	// Запись пустых данных
+	file.seekg(0, file.beg);
+	for (int i = 0; i < 80; i++) {
+		contact emptyContact = { "NULL", "NULL", "8(   )   -    " };
+		file.write((char*)&emptyContact, sizeof(contact));
+	}
+	// Запись заполненных данных
+	file.seekg(0, file.end);
+	for (int i = 0; i < 8; i++) {
+		contact testContact = {"Nikolai", "Kurilov", ""};
+		generatePhone(testContact.phone);
+		file.write((char*)&testContact, sizeof(contact));
+	}
 
-  printFile(file);
-  // Изменение данных
-  file.seekg (ROW_LENGTH * 80 + 2 * NAME_LENGTH, file.beg);
-  file.write("9(999)999-9999", PHONE_LENGTH);
-  file.seekg (ROW_LENGTH * 81, file.beg);
-  file.write("Aaron", NAME_LENGTH);
-  file.seekg (ROW_LENGTH * 83 + NAME_LENGTH, file.beg);
-  file.write("Paul", NAME_LENGTH);
-  file.seekg (ROW_LENGTH * 3 + NAME_LENGTH, file.beg);
-  file.write("Johnson", NAME_LENGTH);
-  printFile(file);
-  file.close();
+	printFile(file, filename);
+	// Изменение данных
+	char dataToWrite[NAME_LENGTH] = {};
+	file.seekg(ROW_LENGTH * 80 + 2 * NAME_LENGTH, file.beg);
+	file.write("8(999)999-9999", PHONE_LENGTH);
+	file.seekg(ROW_LENGTH * 81, file.beg);
+	strcpy_s(dataToWrite, 6, "Aaron");
+	file.write(dataToWrite, NAME_LENGTH);
+	file.seekg(ROW_LENGTH * 83 + NAME_LENGTH, file.beg);
+	strcpy_s(dataToWrite, 5, "Paul");
+	file.write(dataToWrite, NAME_LENGTH);
+	file.seekg(ROW_LENGTH * 3 + NAME_LENGTH, file.beg);
+	strcpy_s(dataToWrite, 8, "Johnson");
+	file.write(dataToWrite, NAME_LENGTH);
+	
+	printFile(file, filename);
+	file.close();
 }
 
 int main() {
-  char fileName[] = "phone.dat";
-  CreateFormatFile(fileName);
+	char fileName[] = "phone.dat";
+	CreateFormatFile(fileName);
+	std::cin.get();
 }
