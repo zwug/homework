7db728f9164d7d61df31ffd1f59e619c708d9e9e
Kirill <zwugme@gmail.com>
2016-01-08 14:47:03 +0300

    Add omp to filtration

--- inform_systems/filtration/main.cpp
+++ inform_systems/filtration/main.cpp
@@ -1,47 +1,57 @@
 #include <iostream>
 #include <complex>
+#include <vector>
 #include <stdlib.h>
+#include <omp.h>
 #include <ctime>
 
 using namespace std;
 
-const int COMPLEX_VALUES = 256;
-const int DOUBLE_VALUES = 500;
+const int X_COUNT = 500;
+const int H_COUNT = 16;
 
-complex<double> * h;
-double * x;
+const int X_SIZE = 10000;
+const int H_SIZE = 256;
 
-void randInit(complex<double> *h, double *x) {
-  for (int j = 0 ; j < COMPLEX_VALUES ; j++) {
-    h[j] = complex<double>( 2 + (rand() % 10) / 10.0, rand() % 20);
-  }
+void randInit(vector<complex<double>> *h, vector<complex<double>> *x) {
+	for (int j = 0; j < H_COUNT; j++) {
+		for (int i = 0; i < H_SIZE; i++) {
+			h[j].push_back(complex<double>(2 + (rand() % 10) / 10.0, rand() % 20));
+		}
+	}
 
-  for (int j = 0 ; j < DOUBLE_VALUES ; j++) {
-    x[j] = (rand() % 1000) / 1000.0;
-  }
+	for (int j = 0; j < X_COUNT; j++) {
+		for (int i = 0; i < X_SIZE; i++) {
+			x[j].push_back(complex<double>(2 + (rand() % 10) / 10.0, rand() % 20));
+		}
+	}
 }
 
-complex<double> filter(complex<double> *h, double *x) {
-  clock_t begin = clock();
-  complex<double> sum = 0;
+complex<double> filter(vector<complex<double>> *h, vector<complex<double>> *x) {
+	complex<double> sum = 0;
 
-  for (int n = 0; n < DOUBLE_VALUES; n++) {
-    for (int m = 0; m < COMPLEX_VALUES; m++) {
-      sum += h[m] * x[n];
-    }
-  }
+	double start_time = omp_get_wtime();
+	#pragma omp parallel for
 
-  clock_t end = clock();
-  cout << "clock ticks passed = " << double(end - begin) << endl;
-  return sum;
+	for (int n = 0; n < H_COUNT; n++) {
+		for (int m = 0; m < H_SIZE; m++) {
+			sum += h[n][m] * x[n][m];
+		}
+	}
+
+	double time = omp_get_wtime() - start_time;
+
+	cout << "time passed = " << time << endl;
+	return sum;
 }
 
 int main()
 {
-    h = new complex<double>[COMPLEX_VALUES]; // input data
-    x = new double [DOUBLE_VALUES]; // input data
+	vector<complex<double>> h[H_COUNT];
+	vector<complex<double>> x[X_COUNT];
 
-    randInit(h, x);
-    cout << "Sum = " << filter(h, x) << endl;
-    return 0;
+	randInit(h, x);
+	cout << "Sum = " << filter(h, x) << endl;
+	cin.get();
+	return 0;
 }
