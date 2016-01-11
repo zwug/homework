5593e438b61887311261afd808255d0358ba736f
Kirill <zwugme@gmail.com>
2016-01-11 23:46:57 +0300

    Added comprasion to single threaded processing

--- inform_systems/filtration/main.cpp
+++ inform_systems/filtration/main.cpp
@@ -10,39 +10,78 @@ using namespace std;
 const int X_COUNT = 500;
 const int H_COUNT = 16;
 
-const int X_SIZE = 10000;
+const int X_SIZE = 1000;
 const int H_SIZE = 256;
 
-void randInit(vector<complex<double>> *h, vector<complex<double>> *x) {
+void randInitMulti(vector<complex<double>> *h, vector<complex<double>> *x) {
+	double start_time = omp_get_wtime();
+	#pragma omp parallel for shared(h)
 	for (int j = 0; j < H_COUNT; j++) {
 		for (int i = 0; i < H_SIZE; i++) {
-			h[j].push_back(complex<double>(2 + (rand() % 10) / 10.0, rand() % 20));
+			h[j].push_back(complex<double>((rand() % 50) / 100.0 - 1, (rand() % 50) / 100.0 - 1));
 		}
 	}
-
+	#pragma omp parallel for shared(x)
 	for (int j = 0; j < X_COUNT; j++) {
 		for (int i = 0; i < X_SIZE; i++) {
-			x[j].push_back(complex<double>(2 + (rand() % 10) / 10.0, rand() % 20));
+			x[j].push_back(complex<double>((rand() % 50) / 100.0 - 1, (rand() % 50) / 100.0 - 1));
 		}
 	}
+
+	double time = omp_get_wtime() - start_time;
+	cout << "INITIALIZTION with 4 threads" << endl;
+	cout << "time passed with 4 threads= " << time << endl;
 }
 
-complex<double> filter(vector<complex<double>> *h, vector<complex<double>> *x) {
-	complex<double> sum = 0;
-
+void randInitSingle(vector<complex<double>> *h, vector<complex<double>> *x) {
 	double start_time = omp_get_wtime();
 	#pragma omp parallel for
+	for (int j = 0; j < H_COUNT; j++) {
+		for (int i = 0; i < H_SIZE; i++) {
+			h[j].push_back(complex<double>((rand() % 50) / 100.0 - 1, (rand() % 50) / 100.0 - 1));
+		}
+	}
+	#pragma omp parallel for
+	for (int j = 0; j < X_COUNT; j++) {
+		for (int i = 0; i < X_SIZE; i++) {
+			x[j].push_back(complex<double>((rand() % 50) / 100.0 - 1, (rand() % 50) / 100.0 - 1));
+		}
+	}
+	double time = omp_get_wtime() - start_time;
+	cout << "INITIALIZTION with single thread" << endl;
+	cout << "time passed = " << time << endl;
+}
 
+complex<double> *filter(vector<complex<double>> *h, vector<complex<double>> *x, complex<double> y[]) {
+	double start_time = omp_get_wtime();
+	#pragma omp parallel for shared(y)
 	for (int n = 0; n < H_COUNT; n++) {
+		y[n] = 0;
 		for (int m = 0; m < H_SIZE; m++) {
-			sum += h[n][m] * x[n][m];
+			y[n] += h[n][m] * x[n][m];
 		}
 	}
 
 	double time = omp_get_wtime() - start_time;
 
-	cout << "time passed = " << time << endl;
-	return sum;
+	cout << "time passed with 4 threads= " << time << endl;
+	return y;
+}
+
+complex<double> *filterSingle(vector<complex<double>> *h, vector<complex<double>> *x, complex<double> y[]) {
+	double start_time = omp_get_wtime();
+
+	for (int n = 0; n < H_COUNT; n++) {
+		y[n] = 0;
+		for (int m = 0; m < H_SIZE; m++) {
+			y[n] += h[n][m] * x[n][m];
+		}
+	}
+
+	double time = omp_get_wtime() - start_time;
+
+	cout << "time passed single threaded= " << time << endl;
+	return y;
 }
 
 int main()
@@ -50,8 +89,15 @@ int main()
 	vector<complex<double>> h[H_COUNT];
 	vector<complex<double>> x[X_COUNT];
 
-	randInit(h, x);
-	cout << "Sum = " << filter(h, x) << endl;
+	randInitMulti(h, x);
+	randInitSingle(h, x);
+	cout << "********CALCULATING***********" << endl;
+	complex<double> *result = new complex<double>[H_COUNT];
+	result = filterSingle(h, x, result);
+	result = filter(h, x, result);
+	for (int i = 0; i < H_COUNT; i++) {
+		cout << "y[" << i << "] = " << result[i] << endl;
+	}
 	cin.get();
 	return 0;
 }
