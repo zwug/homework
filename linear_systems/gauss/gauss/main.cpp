e0cc05e1ac00e71844554f23ba5762cbc7e9ea58
Kirill Mironov <kirill.mironov@ostrovok.ru>
2016-02-22 20:30:37 +0300

    exluded xcodeproj

---
+++ linear_systems/gauss/gauss/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gauss
+//
+//  Created by Kirill Mironov on 21/02/16.
+//  Copyright © 2016 Kirill Mironov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
