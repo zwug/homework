b08c1c76db99e4411d2fb525a677baef7c160544
Kirill <zwugme@gmail.com>
2016-03-20 12:47:38 +0300

    Revert

--- linear_systems/gauss/gauss/main.cpp
+++ linear_systems/gauss/gauss/main.cpp
@@ -1,103 +1,67 @@
 #include <iostream>
 #include <iomanip>
-#include <math.h>
 
-const double eps = 0.001;
 const int N = 3;
 
-bool isConvergent(double A[N][N]) {
-	for (int i = 0; i < N; i++) {
-		double rowSum = 0;
-		double diagonalEl;
-
-		for (int j = 0; j < N; j++) {
-			if (i != j) {
-				rowSum += A[i][j];
-			}
-			else {
-				diagonalEl = A[i][j];
-			}
-		}
-
-		if (rowSum > diagonalEl) {
-			return false;
-		}
-	}
-	return true;
+void printMatrix(double A[N][N + 1]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N + 1; j++) {
+            std::cout << std::setw(10) << A[i][j];
+        }
+        std::cout << '\n';
+    }
+    std::cout << std::endl << std::endl;
 }
 
-void calculate(double A[N][N], double B[N], double *X)
-{
-	double* CurrentX = new double[N];
-	double diff = 1000; // between two iterations
+int main(int argc, const char * argv[]) {
+    double A[N][N + 1] = {
+        {5.4, -2.3, 3.4, -3.5},
+        {4.2, 1.7, -2.3, 2.7},
+        {3.4, 2.4, 7.4, 1.9}
+    };
 
-	while (diff > eps) {
-		for (int i = 0; i < N; i++) {
-			CurrentX[i] = B[i];
-			for (int j = 0; j < N; j++) {
-				if (i != j)
-					CurrentX[i] -= A[i][j] * X[j];
-			}
-			CurrentX[i] /= A[i][i];
-		}
+    double X[N];
 
-		diff = fabs(X[0] - CurrentX[0]);
-		for (int i = 0; i < N; i++) {
-			if (fabs(X[i] - CurrentX[i]) > diff) {
-				diff = fabs(X[i] - CurrentX[i]);
-			}
-			X[i] = CurrentX[i];
-		}
-	}
+    for (int z = 0; z < N; z++) {
+        X[z] = A[z][N];
+    }
 
-	delete[] CurrentX;
-}
+    for (int k = 1; k < N; k++) {
+        for (int j = k; j < N; j++) {
+            double m = A[j][k - 1] / A[k - 1][k - 1];
+            for (int i = 0; i <= N; i++) {
+                A[j][i] -= m * A[k - 1][i];
+            }
+            X[j] -= m * X[k - 1];
+        }
+    }
 
-void print(double A[N][N]) {
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			std::cout << std::setw(10) << A[i][j];
-		}
-		std::cout << '\n';
-	}
-	std::cout << std::endl << std::endl;
-}
+    
+    std::cout << "Matrix A to triangle view =\n";
+    printMatrix(A);
+    
+    std::cout << "X =\n";
 
-void print(double A[N]) {
-	for (int i = 0; i < N; i++) {
-		std::cout << std::setw(10) << A[i];
-		std::cout << '\n';
-	}
-	std::cout << std::endl << std::endl;
-}
+    for (int i = N - 1; i >= 0; i--) {
+        for (int j = i + 1; j < N; j++) {
+            X[i] -= A[i][j] * X[j];
+        }
+        X[i] /= A[i][i];
+    }
 
-int main() {
-	double A[N][N] = {
-		{ 5.4, -2.3, 3.4 },
-		{ 4.2, 1.7, -2.3 },
-		{ 3.4, 2.4, 7.4 }
-	};
-	double B[N] = { -3.5, 2.7, 1.9 };
-	double X[N] = { 0, 1, 0 }; // initial
+    for (int i = 0; i < N; i++) {
+        std::cout << X[i] << '\n';
+    }
 
-	if (isConvergent(A)) {
-		calculate(A, B, X);
-		print(X);
-	}
-	else {
-		std::cout << "The following matrix is not diagonally dominant." << std::endl;
-		print(A);
-		std::cout << "Subtracting row 0 from row 1" << std::endl;
-		for (int i = 0; i < N; i++) {
-			A[1][i] -= A[0][i];
-		}
-		B[1] -= B[0];
+    std::cout << "\n********************* TESTS **********************\n";
 
-		print(A);
-		calculate(A, B, X);
-		print(X);
-	}
-
-	std::cin.ignore();
-	return 0;
+    for (int i = 0; i < N; i++) {
+        double rowSum = 0;
+        for (int j = 0; j < N; j++) {
+            rowSum += A[i][j] * X[j];
+        }
+        std::cout << "Sum of the row number " << i << " = " << rowSum << std::endl;
+        std::cout << "Should be around " << A[i][N] << std::endl << std::endl;
+    }
+    return 0;
 }
